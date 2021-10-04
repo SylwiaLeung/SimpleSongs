6cbdeb2812ad5bef295545f4eff85f368463c9eb
SylwiaLeung <longdepati@gmail.com>
2021-10-04 13:53:32 +0200

    Added method GetSongByTitle

---
+++ SimpleSongs/Controllers/SongController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SimpleSongs.DAO;
+using SimpleSongs.Models;
+
+namespace SimpleSongs.Controllers
+{
+    public class SongController
+    {
+        private IDao<Song> SongDao { get; set; }
+        public SongController(IDao<Song> songDao)
+        {
+            SongDao = songDao;
+        }
+
+        public Song GetSongByTitle(string title)
+        {
+            if(string.IsNullOrEmpty(title))
+            {
+                throw new EmptyTitleException(title);
+            }
+            else return SongDao.Read(title);
+        }
+    }
+}
