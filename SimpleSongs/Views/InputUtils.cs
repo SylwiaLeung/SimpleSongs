a0528e0ac2d4238464e3c2787abb3c990f182ded
SylwiaLeung <longdepati@gmail.com>
2021-10-04 14:34:25 +0200

    Added GetUserInput methods in InputUtils class

---
+++ SimpleSongs/Views/InputUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleSongs.Views
+{
+    public static class InputUtils
+    {
+        public static string GetUserStringInput(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine().Trim();
+            return input;
+        }
+
+        public static double GetUserDoubleInput(string message)
+        {
+            Console.WriteLine(message);
+            double input = Double.Parse(Console.ReadLine().Trim());
+            return input;
+        }
+
+        public static int GetUserIntInput(string message)
+        {
+            Console.WriteLine(message);
+            int input = Int32.Parse(Console.ReadLine().Trim());
+            return input;
+        }
+    }
+}
