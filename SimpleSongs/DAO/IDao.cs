e983e8b5447ad73f6d6c924fd33796a8cc2a1cfa
SylwiaLeung <longdepati@gmail.com>
2021-10-04 12:37:13 +0200

    Added IDao interface

---
+++ SimpleSongs/DAO/IDao.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleSongs.DAO
+{
+    public interface IDao<T>
+    {
+        void Add(T instance);
+        void Update(T instance);
+        void Delete(T instance);
+        T Read(T instance);
+        List<T> ReadAll();
+        void Save();
+    }
+}
