3701b5cb38c64f25fdbb523ea23517fa8dd63a0d
SylwiaLeung <longdepati@gmail.com>
2021-10-04 12:29:35 +0200

    Created a model for Songs

---
+++ SimpleSongs/Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongs.Models
+{
+    public class Song
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string AlbumName { get; set; }
+        public double Length { get; set; }
+    }
+}
