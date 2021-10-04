b9928e0331a7127ab74ea6fa5b3a9bd3c79445ae
SylwiaLeung <longdepati@gmail.com>
2021-10-04 15:00:06 +0200

    Added GeneralMenu class

---
+++ SimpleSongs/Views/Menus/GeneralMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongs.Views.Menus
+{
+    public class GeneralMenu
+    {
+        public void RunMainMenu()
+        {
+            bool isRunning = true;
+            while(isRunning)
+            {
+
+            }
+        }
+        
+        
+    }
+}
